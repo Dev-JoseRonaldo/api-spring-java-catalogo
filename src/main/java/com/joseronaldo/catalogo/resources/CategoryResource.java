f2383e0ccdbe44f0efaa8926989d05bdeaaa19df
José Ronaldo <dev.joseronaldo@gmail.com>
2023-06-19 14:32:29 -0300

    feat: add pagination

--- src/main/java/com/joseronaldo/catalogo/resources/CategoryResource.java
+++ src/main/java/com/joseronaldo/catalogo/resources/CategoryResource.java
@@ -5,6 +5,9 @@ import com.joseronaldo.catalogo.repositories.CategoryRepository;
 import com.joseronaldo.catalogo.services.CategoryService;
 import com.joseronaldo.catalogo.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -23,8 +26,16 @@ public class CategoryResource {
     private CategoryRepository categoryRepository;
 
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll() {
-        List<CategoryDTO> list = categoryService.findAll();
+    public ResponseEntity<Page<CategoryDTO>> findAll(
+        @RequestParam(value = "page", defaultValue = "0") Integer page,
+        @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+        @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+        @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+
+
+        Page<CategoryDTO> list = categoryService.findAllPaged(pageRequest);
         return ResponseEntity.ok().body(list);
     }
 
