d081307a9aa51c620a07740035eb8225110ff2e1
José Ronaldo <dev.joseronaldo@gmail.com>
2023-06-27 16:49:18 -0300

    test: add ProductRepository tests

---
+++ src/test/java/com/joseronaldo/catalogo/repositories/ProductRepositoryTests.java
@@ -0,0 +1,75 @@
+package com.joseronaldo.catalogo.repositories;
+
+import com.joseronaldo.catalogo.entities.Product;
+import com.joseronaldo.catalogo.tests.Factory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+    private long existingId;
+    private long nonExistingId;
+    private long countTotalProduct;
+
+    @Autowired
+    private ProductRepository repository;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProduct = 25;
+    }
+
+    @Test
+    public void deleteShouldDeleteObjectWhenIdExists() {
+
+        repository.deleteById(existingId);
+
+        Optional<Product> result = repository.findById(existingId);
+
+        Assertions.assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void saveShouldPersistWithAutoincrementWhenIdIsNull() {
+
+        Product product = Factory.createProduct();
+        product.setId(null);
+
+        product = repository.save(product);
+
+        Assertions.assertNotNull(product.getId());
+        Assertions.assertEquals(countTotalProduct + 1, product.getId());
+    }
+
+    @Test
+    public void returnOptionalProductNonEmptyWhenExistingId() {
+
+        Optional<Product> result = repository.findById(existingId);
+
+        Assertions.assertTrue(result.isPresent());
+    }
+
+    @Test
+    public void returnOptionalProductEmptyWhenNonExistingId() {
+
+        Optional<Product> result = repository.findById(nonExistingId);
+
+        Assertions.assertFalse(result.isPresent());
+    }
+
+//    @Test
+//    public void deleteShouldThrowEmpityResultResourceNotFoundExceptionWhenIdDoesNotExist() {
+//
+//        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+//            repository.deleteById(nonExistingId);
+//        });
+//    }
+}
